ecbb348ea609b7bb9f6c11e526c1822d97f9f29b
jamfrii <lopezandresgonzalo@gmail.com>
2021-05-18 23:06:14 +0200

    starting

---
+++ list.hpp
@@ -0,0 +1,21 @@
+#include <node.hpp>
+
+node_type
+
+template <typename T>
+class list
+{
+private:
+	/* data */
+public:
+	list(/* args */);
+	~list();
+};
+
+list<T>::list(/* args */)
+{
+}
+
+list::~list()
+{
+}
